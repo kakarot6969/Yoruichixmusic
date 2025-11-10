c287ff43f0f0b6077814760619bb1eed9c01481c
kakarot6969 <rajsrijan62@gmail.com>
2025-11-10 10:19:55 -0800

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ from pyrogram import filters
 load_dotenv()
 
 # ── Core bot config ────────────────────────────────────────────────────────────
-API_ID = int(getenv("API_ID", 27798659))
-API_HASH = getenv("API_HASH", "26100c77cee02e5e34b2bbee58440f86")
+API_ID = int(getenv("API_ID", 37288329))
+API_HASH = getenv("API_HASH", "ff1d5235a4ed929aa15ffbfce36a5395")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 7044783841))
-OWNER_USERNAME = getenv("OWNER_USERNAME", "CertifiedCoder")
-BOT_USERNAME = getenv("BOT_USERNAME", "AnnieXRobot")
-BOT_NAME = getenv("BOT_NAME", "˹𝐀ɴɴɪᴇ ✘ 𝙼ᴜsɪᴄ˼ ♪")
-ASSUSERNAME = getenv("ASSUSERNAME", "musicxannie")
+OWNER_ID = int(getenv("OWNER_ID", 7039972672))
+OWNER_USERNAME = getenv("OWNER_USERNAME", "evokakarot")
+BOT_USERNAME = getenv("BOT_USERNAME", "yoruichixmusicbot")
+BOT_NAME = getenv("BOT_NAME", "˹ʏᴏʀᴜᴄʜɪ 🀄️")
+ASSUSERNAME = getenv("ASSUSERNAME", "Yoruichixmusic")
 
 # ── Database & logging ─────────────────────────────────────────────────────────
 MONGO_DB_URI = getenv("MONGO_DB_URI")
