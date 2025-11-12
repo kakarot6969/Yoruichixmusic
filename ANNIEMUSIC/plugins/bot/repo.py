92e0eba0ea57a2b10c2bb7383ab7eefdcae8433a
kakarot6969 <rajsrijan62@gmail.com>
2025-11-11 22:24:13 -0800

    Update repo.py

--- ANNIEMUSIC/plugins/bot/repo.py
+++ ANNIEMUSIC/plugins/bot/repo.py
@@ -4,36 +4,26 @@ from ANNIEMUSIC import app
 from config import BOT_USERNAME
 
 repo_caption = """**
-🚀 ᴄʟᴏɴᴇ ᴀɴᴅ ᴅᴇᴘʟᴏʏ – ᴄᴇʀᴛɪꜰɪᴇᴅ ᴄᴏᴅᴇʀꜱ ʀᴇᴘᴏ 🚀
+🤡 Dᴇᴀʀ Lᴏʀᴜ, - 
 
-➤ ᴅᴇᴘʟᴏʏ ᴇᴀsɪʟʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴇʀʀᴏʀꜱ  
-➤ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪꜱꜱᴜᴇ  
-➤ ɴᴏ ɪᴅ ʙᴀɴ ɪꜱꜱᴜᴇ  
-➤ ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏꜱ  
-➤ ʀᴜɴ 24/7 ʟᴀɢ ꜰʀᴇᴇ
-
-ɪꜰ ʏᴏᴜ ꜰᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜱꜱ ɪɴ ꜱᴜᴘᴘᴏʀᴛ
+Tᴀʀɪ Mᴀᴀ Kɪ Cʜᴜᴛ Mᴀᴅʜᴇʀᴄʜᴏᴅ... Nᴏɪ Mɪʟᴀ Gᴀ Rᴇᴘᴏ
+sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ᴍᴇʜ ʙʜᴇᴇᴋ ᴍᴀɴɢ ʟᴀᴡ#ᴅᴇ ᴋᴇʜ ᴅᴇᴠʟᴏᴘᴇʀ 🖕
 **"""
 
 @app.on_message(filters.command("repo"))
 async def show_repo(_, msg):
     buttons = [
-        [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [
-            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/CertifiedCoder"),
-            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/CertifiedCodes")
+            InlineKeyboardButton("💬 𝗕𝗛𝗘𝗘𝗞 𝗠𝗔𝗡𝗚", url="https://t.me/Alpha_Bots_Support"),
+            InlineKeyboardButton("🛠️ 𝗨𝗣𝗗𝗔𝗧𝗘 𝗟𝗘𝗛 𝗠𝗖", url="https://t.me/Alpha_Bots_Updates")
         ],
         [
-            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/CertifiedCoders"),
-            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CertifiedDevloper/AnnieXMusic")
+            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/Evokakarot")
         ]
-    ]
-
     reply_markup = InlineKeyboardMarkup(buttons)
 
     try:  
         await msg.reply_photo(
-            photo="https://telegra.ph/file/58afe55fee5ae99d6901b.jpg",
+            photo="https://files.catbox.moe/0aa3v4.jpg",
             caption=repo_caption,
             reply_markup=reply_markup
         )
